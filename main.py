0f7e0785ae52a9e603cca5ca76933aae68e65725
javiermoya2504 <javivemoya@gmail.com>
2025-01-25 12:43:44 -0600

    socker

--- main.py
+++ main.py
@@ -12,9 +12,8 @@ def index():
 @socketio.on('message')
 def handleMessage(msg):
     print('Message: ' + msg)
-    send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=5000)
+    socketio.run(app, debug=True, port=5001)
 
     # pip install flask-socketio
